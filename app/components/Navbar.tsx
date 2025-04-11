78cf0f21e2ac12ace7f66dd90b60ce7bff9480b0
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-04-11 15:37:18 +0900

    added Link to logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,17 +2,20 @@ import { AiOutlineHeart } from "react-icons/ai";
 import Image from "next/image";
 import NavigationMenuBar from "./NavigationMenuBar";
 import LoginDropdown from "./LoginDropdown";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="bg-white flex justify-between px-32 py-4 items-center">
-      <Image
-        src={"/logo.png"}
-        alt="toyota logo"
-        width={130}
-        height={100}
-        className="object-cover"
-      />
+      <Link href={"/"}>
+        <Image
+          src={"/logo.png"}
+          alt="toyota logo"
+          width={130}
+          height={100}
+          className="object-cover hover:opacity-80"
+        />
+      </Link>
       <div className="flex items-center space-x-8">
         <div>
           <NavigationMenuBar />

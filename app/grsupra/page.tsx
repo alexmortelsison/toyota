5e4e9adcae8b094b11c32151307db5d3a0630f9f
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-04-14 20:35:13 +0900

    updated supra page

--- app/grsupra/page.tsx
+++ app/grsupra/page.tsx
@@ -1,12 +1,13 @@
+import Image from "next/image";
 import ModelViewerPage from "../components/ModelViewer";
 
 export default function GrSupraPage() {
   return (
-    <div className="min-h-screen items-center bg-gradient-to-b from bg-slate-300 via-gray-300 to-white">
+    <div className="min-h-screen items-center bg-gradient-to-b from bg-slate-300 via-white to-gray-300">
       <h1 className="text-7xl font-semibold flex justify-center pt-24">
         The ultimate rush hour.
       </h1>
-      <p className="text-2xl w-[42%] mx-auto text-center py-16">
+      <p className="text-2xl w-[42%] mx-auto text-center py-16 text-gray-800">
         Drive into the adrenaline-fueled world of GR Supra, where raw power,
         razor-sharp handling and a race-ready design are geared to subvert your
         every expectation.
@@ -36,6 +37,47 @@ export default function GrSupraPage() {
           <div className="pt-4">
             <h1>NATIONAL AUTO SPORT ASSOCIATION</h1>
             <p className="font-semibold">Complimentary 1-Year Membership *</p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white tracking-tight w-screen mx-auto">
+        <h1 className="text-7xl font-semibold flex justify-center pt-24">
+          Styled to rev up your heartbeat.
+        </h1>
+        <div className="flex items-center">
+          <div>
+            <Image
+              src={"/supra4.avif"}
+              alt="supra4"
+              width={500}
+              height={1000}
+              className="object-cover  rounded-3xl"
+            />
+            <div className="flex space-x-4">
+              <Image
+                src={"/supra4.avif"}
+                alt="supra4"
+                width={500}
+                height={500}
+                className="object-cover rounded-3xl"
+              />
+              <Image
+                src={"/supra4.avif"}
+                alt="supra4"
+                width={500}
+                height={500}
+                className="object-cover rounded-3xl"
+              />
+            </div>
+          </div>
+          <div>
+            <Image
+              src={"/supra4.avif"}
+              alt="supra4"
+              width={1000}
+              height={1000}
+              className="object-cover w-full rounded-3xl"
+            />
           </div>
         </div>
       </div>
